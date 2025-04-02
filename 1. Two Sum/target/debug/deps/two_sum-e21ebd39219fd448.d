6afb0c926e77458607b68c2b514011474dcef48d
unknown <marcos.vinicius.da.silva.pro@gmail.com>
2025-04-02 10:07:10 -0300

    Removed target folder

--- 1. Two Sum/target/debug/deps/two_sum-e21ebd39219fd448.d
+++
@@ -1,9 +0,0 @@
-C:\Users\user\Documents\Pessoal\LeetCode\1. Two Sum\target\debug\deps\libtwo_sum-e21ebd39219fd448.rmeta: src\main.rs Cargo.toml
-
-C:\Users\user\Documents\Pessoal\LeetCode\1. Two Sum\target\debug\deps\two_sum-e21ebd39219fd448.d: src\main.rs Cargo.toml
-
-src\main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
--- 1. Two Sum/target/debug/deps/two_sum.d
+++
@@ -1,5 +0,0 @@
-C:\Users\user\Documents\Pessoal\LeetCode\1. Two Sum\target\debug\deps\two_sum.exe: src\main.rs
-
-C:\Users\user\Documents\Pessoal\LeetCode\1. Two Sum\target\debug\deps\two_sum.d: src\main.rs
-
-src\main.rs:
